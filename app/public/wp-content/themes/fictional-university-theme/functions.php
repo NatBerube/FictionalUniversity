ffdb24db6350f0600eadffafb9343a4b9426374d
Nathaniel <nat_berube@outlook.com>
2024-04-11 16:24:36 -0400

    added a all events archive and a past events page

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -9,15 +9,31 @@ function university_files(){
   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 
 }
-
 add_action('wp_enqueue_scripts', 'university_files');
+
 
 function university_features(){
   add_theme_support('title-tag');
 }
-
 add_action('after_setup_theme', 'university_features');
 
-
+function university_adjust_queries($query){
+  $today = date('Ymd');
+  
+  if (! is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key'     => 'event_date',
+        'compare' => '>=',
+        'value'   => $today,
+        'type'    => 'numeric'
+      )
+    ));
+  }
+}
+add_action('pre_get_posts', 'university_adjust_queries');
 
 
